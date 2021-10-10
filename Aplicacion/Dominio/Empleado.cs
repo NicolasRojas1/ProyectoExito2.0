9ca8d9ece5084c163f7ecfb9956563df1106d294
Nicolas Rojas <nicrojasc6@gmail.com>
2021-10-10 02:20:23 -0500

    Requerimiento para campo Empleado

--- Aplicacion/Dominio/Empleado.cs
+++ Aplicacion/Dominio/Empleado.cs
@@ -1,17 +1,20 @@
-﻿namespace Dominio 
+﻿using System.ComponentModel.DataAnnotations;
+namespace Dominio
 {
 	public class Empleado : Persona
 	{
 		public int EmpleadoId {get; set;}
 		public int CodigoEmpleado {get; set;}
+		[Required(ErrorMessage="El campo Codigo de empleado es requerido")]
 		public string Sucursal {get; set;}
+		[Required(ErrorMessage="El campo Sucursal es requerido")]
 		public string Usuario {get; set;}
+		[Required(ErrorMessage="El campo Usuario es requerido")]
 		public string Contraseña {get; set;}
+		[Required(ErrorMessage="El campo Contraseña es requerido")]
 		public NombreRol NombreRol {get; set;}
-		
 		public void CambioContraseña()
 		{
 		}
 	}
 }
-//falta el acceso requerido
